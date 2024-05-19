2685c8c1de1928dd5b398cc4ca749ee429e743eb
singhharjot011 <singhharjot011@gmail.com>
2024-05-19 02:03:20 -0400

    Hosted API On github

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,3 @@
-export const API_URL = `http://10.0.0.110:5500/api.json`;
+export const API_URL = `https://singhharjot011.github.io/vanillaCRMAPI/api.json`;
 export const NEWS_API_KEY = `214dd065efcd4b55a43916c72b81cc17`;
 export const TIMEOUT_SEC = 10;
