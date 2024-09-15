3f26620f0a56befcc9e59cec36f31a544d270e78
singhharjot011 <singhharjot011@gmail.com>
2024-09-15 14:33:14 -0400

    Added heroku config

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+export default {
+  entry: {
+    main: "./public/js/index.js",
+    login: "./public/js/login.js",
+    updateSettings: "./public/js/updateSettings.js",
+    alerts: "./public/js/alerts.js",
+  },
+  output: {
+    filename: "js/[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",
+    }),
+  ],
+};
